a162c1a032c01eafed3fcdd1269f0d2becd68fb0
ryang2386 <gayler02@nyu.edu>
2025-05-06 22:23:11 -0400

    updated index.ts in back-end

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,13 @@
 import type { Request, Response } from 'express';
 import express from 'express';
+// import { dirname } from 'path';
 const router = express.Router();
 
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+// import { fileURLToPath } from 'node:url';
+// const __dirname = path.dirname(__filename);
+const __dirname = new URL('.', import.meta.url).pathname;
 import apiRoutes from './api/index.js';
 
 router.use('/api', apiRoutes);
